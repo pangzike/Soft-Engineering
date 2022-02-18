a0de9189b6a33e0a069e025deed3f4e46c0ffa81
yang <yangk59@126.com>
2022-02-18 17:52:10 +0800

    first

---
+++ project/project/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  project
+//
+//  Created by 杨可 on 2022/2/18.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
